75bc1554f0a7fe67784fcd99454e5114d88ea318
georgieprat <georgp2906@gmail.com>
2025-05-16 18:39:45 +0200

    frist push

---
+++ js/FormUsage.js
@@ -0,0 +1,10 @@
+const selectElement = document.querySelectorAll('.landDropdown');
+
+dropdowns.forEach(dropdown => {
+    Landliste.forEach(land => {
+      const option = document.createElement("option");
+      option.value = land;
+      option.textContent = land;
+      dropdown.appendChild(option);
+    });
+  });
