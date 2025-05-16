ad58bd3e41c44e5bb569e0fe96c1737eeed6e5b5
georgieprat <georgp2906@gmail.com>
2025-05-16 18:56:57 +0200

    Update FormUsage.js

--- js/FormUsage.js
+++ js/FormUsage.js
@@ -1,10 +1,10 @@
 const dropdowns = document.querySelectorAll('.landDropdown');
 
 dropdowns.forEach(dropdown => {
-    Landliste.forEach(land => {
-      const option = document.createElement("option");
-      option.value = land;
-      option.textContent = land;
-      dropdown.appendChild(option);
+    window.Landliste.forEach(land => {
+        const option = document.createElement("option");
+        option.value = land;
+        option.textContent = land;
+        dropdown.appendChild(option);
     });
   });
