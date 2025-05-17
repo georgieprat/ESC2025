3660d9d50384bc186cdfaa6ad4703ac7e82f5fbb
georgieprat <georgp2906@gmail.com>
2025-05-17 13:05:09 +0200

    Delete js/Landliste.js

--- js/Landliste.js
+++
@@ -1,8 +0,0 @@
-window.Landliste = [
-    "Norwegen", "Luxemburg", "Estland", "Israel", 
-    "Litauen", "Spanien", "Ukraine", "Großbritannien",
-    "Österreich", "Island", "Lettland", "Niederlande",
-    "Finnland", "Italien", "Polen", "Deutschland", "Griechenland",
-    "Armenien", "Schweiz", "Malta", "Portugal", "Dänemark",
-    "Schweden", "Frankreich", "San Marino", "Albanien"
-];
